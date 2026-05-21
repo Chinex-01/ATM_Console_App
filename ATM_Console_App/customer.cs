5e4116dc0cf20cfd66f043ec566f94073f13be5d
Nonso <onyeokorokennedy@gmail.com>
2026-05-21 23:18:20 +0100

    iu

---
+++ ATM_Console_App/customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ATM_Console_App;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+namespace ATM_Console_App
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string AccountNummber { get; set; }
+        public int Pin { get; set; }
+        public string Email { get; set; }
+        public double amount { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
